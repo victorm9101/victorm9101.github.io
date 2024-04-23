5318d6aac0cfed7591965fd1af0f717311063fdb
victorm9101 <162838160+victorm9101@users.noreply.github.com>
2024-04-23 14:30:42 -0500

    initial commit

---
+++ validacion.js
@@ -0,0 +1,11 @@
+
+
+function boton() {
+    let name = document.querySelectorAll('.formcontato__input')
+    for (let index = 0; index < name.length; index++) {
+        alert(name[index].value);
+        
+    }
+
+}
+
